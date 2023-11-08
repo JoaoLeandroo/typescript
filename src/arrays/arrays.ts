4af29cb20f8420d70ab1088ef7f03ec941c10a98
joaoleandro <new.joaoleandro@gmail.com>
2023-11-08 14:58:55 -0300

    configurando o code runner, para executar o typescript diretamente no terminal

--- src/arrays/arrays.ts
+++ src/arrays/arrays.ts
@@ -7,3 +7,6 @@ let tipos: Array<boolean> = [true, false, true, true, true, false]
 // irá receber apenas esses valores. *Apenas os 3, caso mais valores forem adicionados irá acontecer um erro
 let teste: [string, number, boolean] = ['joao', 20, true]
 
+
+console.log(teste)
+
