517fa189b3f6ce7eeb80a8617ea1d612ab08ce39
joaoleandro <new.joaoleandro@gmail.com>
2023-11-29 17:26:21 -0300

    testando objetos em array

--- src/objetos/objeto.ts
+++ src/objetos/objeto.ts
@@ -11,5 +11,15 @@ let objetoTwo: Record<string, string>
 objetoTwo = {
     teste1: '',
     teste2: '',
-    teste3: ''
+    teste3: '',
 }
+
+interface Carro {
+    modelo: string
+    ano: number
+}
+
+const carrosX: Carro[] = []
+carrosX.push({modelo: "Ferrari", ano: 2022})
+console.log(carrosX)
+
