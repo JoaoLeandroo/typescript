9b52c1bc9fb5e5e25a3f9b74818f3cde5f5590d3
joaoleandro <new.joaoleandro@gmail.com>
2023-11-29 20:24:07 -0300

    assertions

---
+++ dist/assertions/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let statusAtual = 10;
+console.log(statusAtual);
+let novoStatus = statusAtual;
+console.log(novoStatus);
+//# sourceMappingURL=index.js.map
---
+++ src/assertions/index.ts
@@ -0,0 +1,8 @@
+
+let statusAtual: unknown = 10;
+
+console.log(statusAtual)
+
+let novoStatus:number = statusAtual as number
+
+console.log(novoStatus)
