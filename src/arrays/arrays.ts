2bb2d016c6105e06a6debd6123ec4d44ded06e10
joaoleandro <new.joaoleandro@gmail.com>
2023-11-29 15:42:22 -0300

    array

--- src/arrays/arrays.ts
+++ src/arrays/arrays.ts
@@ -7,6 +7,11 @@ let tipos: Array<boolean> = [true, false, true, true, true, false]
 // irá receber apenas esses valores. *Apenas os 3, caso mais valores forem adicionados irá acontecer um erro
 let teste: [string, number, boolean] = ['joao', 20, true]
 
+teste.push(234234)
 
 console.log(teste)
 
+let filme: Array<string | number> = []
+
+filme.push("Vingadores")
+filme.push(22)
